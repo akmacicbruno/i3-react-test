0e59568275d921010764c709231ac39acb8ca115
bruno <akmacicbruno@gmail.com>
2024-10-09 18:53:07 +0200

    Dodane nove rute

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Components/Layout";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
