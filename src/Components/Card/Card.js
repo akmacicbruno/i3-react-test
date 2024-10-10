85ffb18b3c05a00652fdc7e74ee0b24bfe079332
bruno <akmacicbruno@gmail.com>
2024-10-10 11:57:43 +0200

    Dodane kartice i stilovi za kartice

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style-card.css";
+
+const Card = ({ name, description, imageUrl }) => {
+  return (
+    <div className="card">
+      <img src={imageUrl} alt={name} className="card-img" />
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p className="card-description">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
