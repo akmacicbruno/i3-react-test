e8241b3e21465a7f1d4620feaf93e0647dc5adda
bruno <akmacicbruno@gmail.com>
2024-10-10 14:09:34 +0200

    Uređena about stranica, dodani stilovi

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,10 +1,62 @@
 import "./style-about.css";
+import { useEffect, useState } from "react";
 
 function About() {
+  const [airportData, setAirportData] = useState([]);
+
+  useEffect(() => {
+    // Fetch the JSON data
+    fetch("/airport.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setAirportData(data.airport))
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  }, []);
+
   return (
-    <div className="about">
-      <p>this is about page</p>
-    </div>
+    <>
+      <div className="container-about">
+        <div>
+          <h1 className="title">{airportData.name}</h1>
+        </div>
+        <div>
+          <br />
+        </div>
+      </div>
+
+      <div className="about">
+        <div className="about__left">
+          <p className="about__left-paragraph">{airportData.description}</p>
+          <div className="about__left-cards">
+            {airportData.features?.map((feature, index) => (
+              <div key={index} className="about__left-cards-item">
+                <img
+                  className="about__left-cards-item-img"
+                  src={feature.img}
+                  alt="img"
+                />
+                <p className="about__left-cards-item-paragraph">
+                  <strong>{feature.feature}</strong>: {feature.description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="about__right">
+          <img
+            className="about__right-img"
+            src={airportData.imgUrl}
+            alt="img"
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
